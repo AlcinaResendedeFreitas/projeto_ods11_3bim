a04e8e03d9d1ba0489a5e520150f13a6053618f9
augusto <augustotognetti@hotmail.com>
2022-11-20 17:51:06 -0300

    xml e js 1.5

--- js/funcoes.js
+++ js/funcoes.js
@@ -3,9 +3,112 @@ xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET","xml/conteudo.xml",false);
 xmlhttp.send();
 xmlDoc = xmlhttp.responseXML;
-var texto_xml = xmlDoc.getElementsByTagName("texto");
-var imagem_xml = xmlDoc.getElementsByTagName("imagem");
+var xml_texto = xmlDoc.getElementsByTagName("texto");
 
-function mostrarTexto(){
-    document.write(texto_xml[1]);
+function mostrarTexto1(){
+    document.write(xml_texto[0].childNodes[0].nodeValue);
+}
+
+function mostrarTexto2(){
+    document.write(xml_texto[1].childNodes[0].nodeValue);
+}
+
+function mostrarTexto3(){
+    document.write(xml_texto[2].childNodes[0].nodeValue);
+}
+
+function mostrarTexto4(){
+    document.write(xml_texto[3].childNodes[0].nodeValue);
+}
+
+function mostrarTexto5(){
+    document.write(xml_texto[4].childNodes[0].nodeValue);
+}
+
+function mostrarTexto6(){
+    document.write(xml_texto[5].childNodes[0].nodeValue);
+}
+
+function mostrarTexto7(){
+    document.write(xml_texto[6].childNodes[0].nodeValue);
+}
+
+function mostrarTexto8(){
+    document.write(xml_texto[7].childNodes[0].nodeValue);
+}
+
+function mostrarTexto9(){
+    document.write(xml_texto[8].childNodes[0].nodeValue);
+}
+
+function mostrarTexto10(){
+    document.write(xml_texto[9].childNodes[0].nodeValue);
+}
+
+function mostrarTexto11(){
+    document.write(xml_texto[10].childNodes[0].nodeValue);
+}
+
+function mostrarTexto12(){
+    document.write(xml_texto[11].childNodes[0].nodeValue);
+}
+
+function mostrarTexto13(){
+    document.write(xml_texto[12].childNodes[0].nodeValue);
+}
+
+function mostrarTexto14(){
+    document.write(xml_texto[13].childNodes[0].nodeValue);
+}
+
+function mostrarTexto15(){
+    document.write(xml_texto[14].childNodes[0].nodeValue);
+}
+
+function mostrarTexto16(){
+    document.write(xml_texto[15].childNodes[0].nodeValue);
+}
+
+function mostrarTexto17(){
+    document.write(xml_texto[16].childNodes[0].nodeValue);
+}
+
+function mostrarTexto18(){
+    document.write(xml_texto[17].childNodes[0].nodeValue);
+}
+
+function mostrarTexto19(){
+    document.write(xml_texto[18].childNodes[0].nodeValue);
+}
+
+function mostrarTexto20(){
+    document.write(xml_texto[19].childNodes[0].nodeValue);
+}
+
+function mostrarTexto21(){
+    document.write(xml_texto[20].childNodes[0].nodeValue);
+}
+
+function mostrarTexto22(){
+    document.write(xml_texto[21].childNodes[0].nodeValue);
+}
+
+function mostrarTexto23(){
+    document.write(xml_texto[22].childNodes[0].nodeValue);
+}
+
+function mostrarTexto24(){
+    document.write(xml_texto[23].childNodes[0].nodeValue);
+}
+
+function mostrarTexto25(){
+    document.write(xml_texto[24].childNodes[0].nodeValue);
+}
+
+function mostrarTexto26(){
+    document.write(xml_texto[25].childNodes[0].nodeValue);
+}
+
+function mostrarTexto27(){
+    document.write(xml_texto[26].childNodes[0].nodeValue);
 }
