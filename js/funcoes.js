fa0e52a27d025bb65069c180f9f1a8e191ec858d
augusto <augustotognetti@hotmail.com>
2022-11-20 19:08:18 -0300

    xml e js 2.0

--- js/funcoes.js
+++ js/funcoes.js
@@ -4,9 +4,25 @@ xmlhttp.open("GET","xml/conteudo.xml",false);
 xmlhttp.send();
 xmlDoc = xmlhttp.responseXML;
 var xml_texto = xmlDoc.getElementsByTagName("texto");
+var xml_imagem = xmlDoc.getElementsByTagName("imagem");
 
+function mostrarImagem1(){
+    document.write("<img src='imgs/" + xml_imagem[0].childNodes[0].nodeValue + "' class='d-inline-block' width='72' height='72'>");
+}
 function mostrarTexto1(){
     document.write(xml_texto[0].childNodes[0].nodeValue);
+}
+
+function mostrarImagem2(){
+    document.write("<img src='imgs/" + xml_imagem[1].childNodes[0].nodeValue + "' class='d-block w-100' alt='...'></img>")
+}
+
+function mostrarImagem3(){
+    document.write("<img src='imgs/" + xml_imagem[2].childNodes[0].nodeValue + "' class='d-block w-100' alt='...'></img>")
+}
+
+function mostrarImagem4(){
+    document.write("<img src='imgs/" + xml_imagem[3].childNodes[0].nodeValue + "' class='d-block w-100' alt='...'></img>")
 }
 
 function mostrarTexto2(){
@@ -21,16 +37,36 @@ function mostrarTexto4(){
     document.write(xml_texto[3].childNodes[0].nodeValue);
 }
 
+function mostrarImagem5(){
+    document.write("<img class='img-responsive rounded' src='imgs/" + xml_imagem[4].childNodes[0].nodeValue + "' width='100%'>")
+}
+
+function mostrarImagem6(){
+    document.write("<img class='img-responsive rounded' src='imgs/" + xml_imagem[5].childNodes[0].nodeValue + "' width='100%'>")
+}
+
 function mostrarTexto5(){
     document.write(xml_texto[4].childNodes[0].nodeValue);
+}
+
+function mostrarImagem7(){
+    document.write("<img class='card-img-top' src='imgs/" + xml_imagem[6].childNodes[0].nodeValue + "' alt='Card image cap'>")
 }
 
 function mostrarTexto6(){
     document.write(xml_texto[5].childNodes[0].nodeValue);
 }
 
+function mostrarImagem8(){
+    document.write("<img class='card-img-top' src='imgs/" + xml_imagem[7].childNodes[0].nodeValue + "' alt='Card image cap'>")
+}
+
 function mostrarTexto7(){
     document.write(xml_texto[6].childNodes[0].nodeValue);
+}
+
+function mostrarImagem9(){
+    document.write("<img class='card-img-top' src='imgs/" + xml_imagem[8].childNodes[0].nodeValue + "' alt='Card image cap'>")
 }
 
 function mostrarTexto8(){
@@ -41,12 +77,24 @@ function mostrarTexto9(){
     document.write(xml_texto[8].childNodes[0].nodeValue);
 }
 
+function mostrarImagem10(){
+    document.write("<img src='imgs/" + xml_imagem[9].childNodes[0].nodeValue + "' class='d-block w-100' alt='...'>");
+}
+
 function mostrarTexto10(){
     document.write(xml_texto[9].childNodes[0].nodeValue);
 }
 
+function mostrarImagem11(){
+    document.write("<img src='imgs/" + xml_imagem[10].childNodes[0].nodeValue + "' class='d-block w-100' alt='...'>");
+}
+
 function mostrarTexto11(){
     document.write(xml_texto[10].childNodes[0].nodeValue);
+}
+
+function mostrarImagem12(){
+    document.write("<img src='imgs/" + xml_imagem[11].childNodes[0].nodeValue + "' class='d-block w-100' alt='...'>");
 }
 
 function mostrarTexto12(){
@@ -57,12 +105,24 @@ function mostrarTexto13(){
     document.write(xml_texto[12].childNodes[0].nodeValue);
 }
 
+function mostrarImagem13(){
+    document.write("<img src='imgs/" + xml_imagem[12].childNodes[0].nodeValue + "' class='d-block w-100' alt='...'>");
+}
+
 function mostrarTexto14(){
     document.write(xml_texto[13].childNodes[0].nodeValue);
 }
 
+function mostrarImagem14(){
+    document.write("<img src='imgs/" + xml_imagem[13].childNodes[0].nodeValue + "' class='d-block w-100' alt='...'>");
+}
+
 function mostrarTexto15(){
     document.write(xml_texto[14].childNodes[0].nodeValue);
+}
+
+function mostrarImagem15(){
+    document.write("<img src='imgs/" + xml_imagem[14].childNodes[0].nodeValue + "' class='d-block w-100' alt='...'>");
 }
 
 function mostrarTexto16(){
@@ -71,6 +131,10 @@ function mostrarTexto16(){
 
 function mostrarTexto17(){
     document.write(xml_texto[16].childNodes[0].nodeValue);
+}
+
+function mostrarImagem16(){
+    document.write("<img class='bi me-2' src='imgs/" + xml_imagem[15].childNodes[0].nodeValue + "' width='160' height='135'>")
 }
 
 function mostrarTexto18(){
